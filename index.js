06de47b15dcbd156b1c471c5071d0c3572f52291
forkermarcel-eng <forker.marcel@icloud.com>
2026-08-19 16:02:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Marcel WhatsApp Bot läuft.");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server läuft auf Port ${port}`);
+});
